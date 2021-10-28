5637f2526a8d8c8f01ca35e62427e398660aba28
Sheikh Alif <salif30@stuy.edu>
2021-10-27 23:30:55 -0400

    finding elements

--- stuyAct.py
+++ stuyAct.py
@@ -1,6 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 PATH = "C:\Program Files\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://techwithtim.net")
+driver.get("https://stuyactivities.org/catalog")
+
+time.sleep(20);
+
+main = driver.find_element_by_class_name("my-masonry-grid_column")
+
+print(main.text)
+
+time.sleep(2)
+
+driver.quit()
