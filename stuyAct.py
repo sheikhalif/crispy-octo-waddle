ea7b07b1f1f053207281a1a036ad15ee7cadc6c0
Sheikh Alif <salif30@stuy.edu>
2021-10-31 11:37:13 -0400

    everything ready to go, run to get full results

--- stuyAct.py
+++ stuyAct.py
@@ -18,20 +18,20 @@ time.sleep(20)
 allClubs = driver.find_elements(By.CLASS_NAME, "jss4")
 print(len(allClubs))
 
-"""
+
 for i in range (len(allClubs)):
     hyperlink = allClubs[i+1].get_attribute("href")
     driver2 = webdriver.Chrome(PATH)
     driver2.get(hyperlink + "/members")
     time.sleep(2)
     clubName = driver2.find_element_by_css_selector(".jss18")
-    print("Club: " + clubName.text + "\Members are:")
+    print("Club: " + clubName.text + "\nMembers are:")
     namesList = driver2.find_elements(By.CSS_SELECTOR, ".MuiTypography-body1")
     for x in range(len(namesList)-5):
         print(namesList[x+5].text)
     print("\n")
     driver2.close()
-"""
+
 time.sleep(2)
 driver.close()
 #sys.stdout.close()
